4b7fb7025e45288d5b76236132df17cc60fc8935
mmupfumir <mmupfumir@hartford.edu>
2024-08-08 01:01:01 -0400

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from "@/sections/Header";
+import { LogoTicker } from "@/sections/LogoTicker";
+
+export default function Home() {
+  return (
+    <>
+      <Header />;
+      <LogoTicker />
+    </>
+
+  );
+}
